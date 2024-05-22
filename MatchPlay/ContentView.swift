5e5bd07cc28d8ac582e339507dbc27397316feed
Rahulk284 <rahulk28582@gmail.com>
2024-05-22 01:06:27 -0500

    Initial Commit

---
+++ MatchPlay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatchPlay
+//
+//  Created by Rahul Kumar on 5/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatchPlay/MatchPlayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchPlayApp.swift
+//  MatchPlay
+//
+//  Created by Rahul Kumar on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MatchPlayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
